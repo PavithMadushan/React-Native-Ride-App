d8068abeb39f935d0441449cf2a788ea24a4d0ca
PavithMadushan <pavithmadushan@gmail.com>
2024-11-13 00:35:22 +0530

    first commit

---
+++ app/(auth)/sign-in.tsx
@@ -0,0 +1,16 @@
+/* eslint-disable prettier/prettier */
+import React from "react";
+import { View, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+
+const SignIn = () => {
+  
+  return (
+    <SafeAreaView>
+        <Text>SignIn</Text>
+    </SafeAreaView>
+  )
+};
+
+export default SignIn;
--- app/+html.tsx
+++ app/+html.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 import { ScrollViewStyleReset } from 'expo-router/html';
 import { type PropsWithChildren } from 'react';
 
